0fed564aa186edab57bfdb540898178c4182527f
MaGeD01-eng <momedmaged0@gmail.com>
2026-06-21 00:09:42 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
 
 // رابط السيرفر الأساسي بتاع الـ Backend
 // لو غيرت البورت في ملف .env بتاع السيرفر، عدّل هنا بنفس القيمة
-const BASE_URL = "https://ecommerce-lueqea2j.b4a.run/";
+const BASE_URL = "https://ecommerce-vq76q8tx.b4a.run/";
 
 // أسماء المفاتيح المستخدمة في localStorage (في مكان واحد عشان منغلطش)
 const STORAGE_KEYS = {
